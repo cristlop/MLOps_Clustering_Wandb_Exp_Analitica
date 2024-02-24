22309d58d28051d1c6a1d7d9e59206a4cac0cf1e
cristlop <144555624+cristlop@users.noreply.github.com>
2024-02-24 11:37:44 -0500

    Update load.py

--- src/data/load.py
+++ src/data/load.py
@@ -5,6 +5,7 @@ from sklearn import datasets
 import argparse
 import wandb
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--IdExecution', type=str, help='ID of the execution')
 args = parser.parse_args()
