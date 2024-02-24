8d54311561f7776ad37b78da817f40466ca0f2c0
cristlop <144555624+cristlop@users.noreply.github.com>
2024-02-24 11:30:35 -0500

    Update load.py

--- src/data/load.py
+++ src/data/load.py
@@ -1,11 +1,9 @@
 import torch
 import torchvision
 from torch.utils.data import TensorDataset
+from sklearn import datasets
 import argparse
 import wandb
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--IdExecution', type=str, help='ID of the execution')
@@ -15,43 +13,44 @@ if args.IdExecution:
     print(f"IdExecution: {args.IdExecution}")
 
 def load(train_size=.8):
-    """
-    # Load the data
-    """
-      
-    # Load the Iris dataset
-    iris = datasets.load_iris()
-    X, y = iris.data, iris.target
+    # Load the Wisconsin Breast Cancer dataset
+    wbcd = datasets.load_breast_cancer()
+    feature_names = wbcd.feature_names
+    labels = wbcd.target_names
+
+    x, y = torch.tensor(wbcd.data).float(), torch.tensor(wbcd.target)
 
     # split off a validation set for hyperparameter tuning
-    X_train, X_val, y_train, y_val = train_test_split(X, y, train_size=train_size, random_state=42)
+    split_idx = int(len(x) * train_size)
+    x_train, x_val = x[:split_idx], x[split_idx:]
+    y_train, y_val = y[:split_idx], y[split_idx:]
 
-    training_set = TensorDataset(torch.Tensor(X_train), torch.Tensor(y_train))
-    validation_set = TensorDataset(torch.Tensor(X_val), torch.Tensor(y_val))
+    training_set = TensorDataset(x_train, y_train)
+    validation_set = TensorDataset(x_val, y_val)
     datasets = [training_set, validation_set]
     return datasets
 
 def load_and_log():
     # 🚀 start a run, with a type to label it and a project it can call home
     with wandb.init(
-        project="MLOps-Pycon2023",
+        project="MLOps-2024",
         name=f"Load Raw Data ExecId-{args.IdExecution}", job_type="load-data") as run:
-        
+
         datasets = load()  # separate code for loading the datasets
         names = ["training", "validation"]
 
         # 🏺 create our Artifact
         raw_data = wandb.Artifact(
-            "iris-raw", type="dataset",
-            description="raw Iris dataset, split into train/val",
-            metadata={"source": "sklearn.datasets.load_iris",
+            "wisconsin-breast-cancer-raw", type="dataset",
+            description="raw Wisconsin Breast Cancer dataset, split into train/val",
+            metadata={"source": "sklearn.datasets.load_breast_cancer",
                       "sizes": [len(dataset) for dataset in datasets]})
 
         for name, data in zip(names, datasets):
             # 🐣 Store a new file in the artifact, and write something into its contents.
             with raw_data.new_file(name + ".pt", mode="wb") as file:
-                X, y = data.tensors
-                torch.save((X, y), file)
+                x, y = data.tensors
+                torch.save((x, y), file)
 
         # ✍️ Save the artifact to W&B.
         run.log_artifact(raw_data)
