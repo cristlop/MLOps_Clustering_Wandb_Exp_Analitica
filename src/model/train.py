18ec4be5a790fc783e02603dab7923919d0e14de
cristlop <144555624+cristlop@users.noreply.github.com>
2024-02-29 18:50:39 -0500

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -2,16 +2,15 @@ import torch
 import torch.nn.functional as F
 from torch import nn 
 from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import plot_roc_curve, plot_precision_recall_curve
 from sklearn.utils import class_weight
-
 import os
 import argparse
 import wandb
-from torch.utils.data import DataLoader
 
 # Load data
 wbcd = load_breast_cancer()
@@ -167,6 +166,4 @@ def evaluate_and_log(experiment_id='99', config=None, model=None, X_test=None, y
     with wandb.init(project="MLOps-2024", name=f"Eval Model ExecId-{args.IdExecution} Experiment-{experiment_id}"):
         data = run.use_artifact('mnist-preprocess:latest')
         data_dir = data.download()
-        testing_set = read(data_dir, "test")
-
-        test_loader = torch.utils.data.DataLoader
+        testing
